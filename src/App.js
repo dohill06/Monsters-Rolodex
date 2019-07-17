2dbfce0217f3be9a7e18f0789315dd8d89e6adf4
Douglas Hill <dohill06@gmail.com>
2019-07-17 16:43:38 -0700

    Change App.js to class component and add initial state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+class App extends Component {
+  state = {
+    monsters: [
+      {
+        name: "Frankenstein",
+        id: "asc1"
+      },
+      {
+        name: "Dracula",
+        id: "asr2"
+      },
+      {
+        name: "Zombie",
+        id: "as1w"
+      }
+    ]
+  };
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.monsters.map(monster => (
+          <h1 key={monster.id}> {monster.name} </h1>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
