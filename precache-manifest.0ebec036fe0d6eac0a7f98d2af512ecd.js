e4b0ac3d6c725a340e92141aa6670a581461e37f
Douglas Hill <dohill06@gmail.com>
2019-07-19 10:40:24 -0700

    Updates

---
+++ precache-manifest.0ebec036fe0d6eac0a7f98d2af512ecd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8933c62bb96aa0f1f7eb9d20f5ae277",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "858020f96c7ae4451304",
+    "url": "/Monsters-Rolodex/static/css/main.b3c40bc4.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/Monsters-Rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "858020f96c7ae4451304",
+    "url": "/Monsters-Rolodex/static/js/main.01eed41d.chunk.js"
+  },
+  {
+    "revision": "3c42c109e430c626051d",
+    "url": "/Monsters-Rolodex/static/js/runtime~main.2f1dcf62.js"
+  }
+]);
